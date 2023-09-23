775ea582af2a40fdf8be85d5e8b0c7a8446358a1
Tanisha Pattnaik <tanishakhuc@gmail.com>
2023-09-23 19:13:14 +0530

    homepage correction

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div>
         {/* <Navbar/> */}
-        <div className='content'>
+        <div className='contenthome1'>
         <img src='https://i.postimg.cc/28C2mYG3/college-project-rafiki.png' height="500px" weight="400px"></img>
         <div className='contenthome'><h1>AICTE Curriculum Portal</h1>
           <p>This portal is collective platform for educators, curriculum designers and administrators as well</p>
