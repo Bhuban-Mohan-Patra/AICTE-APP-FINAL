7fbdcd2eb13dfb333ba3274ebb91a202dba54330
Tanisha Pattnaik <tanishakhuc@gmail.com>
2023-09-23 19:21:16 +0530

    homecontent

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,7 +10,11 @@ function Home() {
         <div className='contenthome1'>
         <img src='https://i.postimg.cc/28C2mYG3/college-project-rafiki.png' height="500px" weight="400px"></img>
         <div className='contenthome'><h1>AICTE Curriculum Portal</h1>
-          <p>This portal is collective platform for educators, curriculum designers and administrators as well</p>
+          <p>Being a collective platform for educators, curriculum designers and administrators,
+          the portal represents a pivotal step towards ensuring a consistent and high-quality technical education experience for students across India. </p>
+          <p>This unified portal will serve as a central platform for curriculum development, collaboration among educational experts, and the continuous improvement of technical education standards.</p>
+          <p>The portal's online nature ensures accessibility to educational experts, curriculum designers, and institutions regardless of their geographical location. </p>
+          <p>With a strong emphasis on data security and scalability, the portal has the potential to shape the future of technical education in India, making it more responsive, relevant, and inclusive</p>
         </div>
         </div>
         {/* <Footer/> */}
