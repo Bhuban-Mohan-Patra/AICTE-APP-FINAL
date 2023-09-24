4d2073d41d105dd95d2ac72c30381f1d14bba539
Your Name <you@example.com>
2023-09-24 23:08:43 +0530

    handled alerts

--- server/controllers/handleFeedback.js
+++ server/controllers/handleFeedback.js
@@ -5,6 +5,7 @@ const handleFeedback=async (req,res)=>
     console.log(req.body);
     const desn=await Designer.updateOne({_id:req.body.c_creator},{$push:{feedbacks:req.body}});
     console.log(desn);
+    res.status(201).json('Feedback updated');
 
 }
 
